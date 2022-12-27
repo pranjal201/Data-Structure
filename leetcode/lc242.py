85a1e5b90c54825ad0489cf5eec590e6cbc3aadf
pranjal201 <pranjalparmar2001@gmail.com>
2022-12-27 17:03:30 +0530

    solved the anagram problem

---
+++ leetcode/lc242.py
@@ -0,0 +1,28 @@
+# 242. Valid Anagram
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+# Example 1:
+# Input: s = "anagram", t = "nagaram"
+# # Output: true
+def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        countS = {}
+        countT = {}
+
+        for i in range(0,len(s)):
+            countS[s[i]] = 1 + countS.get(s[i],0)
+            countT[t[i]] = 1 + countT.get(t[i],0)
+        
+        for k in countS:
+            if countS[k] != countT.get(k,0):
+                return False
+        return True
+if __name__ == '__main__':
+    s = 'top'
+    t = 'pot'
+    print(isAnagram(s,t))
